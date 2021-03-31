75d70e9383f492c66263d84c423a7374b40122ef
sajathraffeekQA <MMohamedRaffeek@qa.com>
2021-03-31 15:11:26 +0100

    uploaded operator exercise

---
+++ src/com/qa/day1/Runner.java
@@ -0,0 +1,15 @@
+package com.qa.day1;
+
+public class Runner {
+	public static void main(String[] args) {
+	
+	int r1 = exercise.add(3,4);
+	System.out.println(r1);
+
+	int r2 = exercise.multiply(12,6);
+	System.out.println(r2);
+	 
+	int r3 = exercise.sub(9, 6);
+	System.out.println(r3);
+	}
+}
---
+++ src/com/qa/day1/exercise.java
@@ -0,0 +1,20 @@
+package com.qa.day1;
+
+public class exercise {
+	public static int add(int num1, int num2) {
+		int result1 = num1 + num2;
+		return result1;
+	}
+
+	public static int multiply(int num3, int num4) {
+		int result2 = num3 * num4;
+		return result2;
+	}
+
+	public static int sub(int num5, int num6) {
+		int result3 = num5 + num6;
+		return result3;
+	}
+
+
+}
